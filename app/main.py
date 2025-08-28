56c23ac4d5b78d39aaa24a34eb6bcea3ce9d18b4
Yurii <yurij.kenu@gmail.com>
2025-08-28 16:24:22 +0200

    comprehension: using one function into another

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-
-
 def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
@@ -12,13 +10,9 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [{
-            "line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8",
-        } for error in errors
+        "errors": [
+            format_linter_error(error)
+            for error in errors
         ],
         "path": file_path,
         "status": "failed" if errors else "passed",
@@ -27,15 +21,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [{
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8",
-            } for error in errors],
-            "path": path,
-            "status": "failed" if errors else "passed",
-        } for path, errors in linter_report.items()
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
     ]
